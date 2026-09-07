aea82b49e648d83f3764f580bff16293debbe0e9
AhmedRayan254 <ahmed.rafat.rayan@gmail.com>
2026-09-07 10:26:57 +0300

    Add ROS 2 workspace and robot_arm_description package

---
+++ build/robot_arm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/ros2_robot_arm_ws/src/robot_arm_description
+# Build directory: /home/ahmed/ros2_robot_arm_ws/build/robot_arm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ install/robot_arm_description/share/robot_arm_description/local_setup.bash
@@ -0,0 +1,1 @@
+/home/ahmed/ros2_robot_arm_ws/build/robot_arm_description/ament_cmake_environment_hooks/local_setup.bash
